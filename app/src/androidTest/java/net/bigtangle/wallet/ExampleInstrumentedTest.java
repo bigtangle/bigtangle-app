c9b49fca1aeee04bf35600f411aafa9e67c750b9
jiangtao <jiangtao19890902@sina.com>
2019-07-06 22:31:26 +0800

    build gradle

--- app/src/androidTest/java/net/bigtangle/wallet/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/net/bigtangle/wallet/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.eletac.tronwallet;
+package net.bigtangle.wallet;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -9,18 +9,11 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.eletac.tronwallet", appContext.getPackageName());
+        assertEquals("net.bigtangle.wallet", appContext.getPackageName());
     }
 }
