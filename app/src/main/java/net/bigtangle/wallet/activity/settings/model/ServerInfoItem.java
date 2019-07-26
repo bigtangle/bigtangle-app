39ea830e8e1a703a8286e940d0a12d6867e2407a
姜涛 <jiangtao19890902@sina.com>
2019-07-26 14:54:33 +0800

    整理代码

---
+++ app/src/main/java/net/bigtangle/wallet/activity/settings/model/ServerInfoItem.java
@@ -0,0 +1,24 @@
+package net.bigtangle.wallet.activity.settings.model;
+
+public class ServerInfoItem {
+
+    private String serverName;
+
+    private String connectionURL;
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getConnectionURL() {
+        return connectionURL;
+    }
+
+    public void setConnectionURL(String connectionURL) {
+        this.connectionURL = connectionURL;
+    }
+}
