d7c64aa7cd5ea7663eef4ece0e9110ac52cdaa45
jiangtao <jiangtao19890902@sina.com>
2019-08-06 23:28:09 +0800

    权限处理

--- app/src/main/java/net/bigtangle/wallet/activity/ImportWalletActivity.java
+++ app/src/main/java/net/bigtangle/wallet/activity/ImportWalletActivity.java
@@ -1,21 +1,13 @@
 package net.bigtangle.wallet.activity;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.leon.lfilepickerlibrary.LFilePicker;
 import com.yarolegovich.lovelydialog.LovelyInfoDialog;
@@ -29,13 +21,10 @@ import net.bigtangle.wallet.core.utils.WalletFileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class ImportWalletActivity extends AppCompatActivity {
 
@@ -64,7 +53,6 @@ public class ImportWalletActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_wallet);
         ButterKnife.bind(this);
-        myRequetPermission();
         this.initView();
     }
 
@@ -144,14 +132,8 @@ public class ImportWalletActivity extends AppCompatActivity {
             public void onClick(View v) {
                 WalletContextHolder walletContextHolder = WalletContextHolder.get();
                 if (!walletContextHolder.checkWalletExists()) {
-                    try {
-                        WalletFileUtils.createWalletFileAndLoad();
-                    } catch (IOException e) {
-                        Log.e(LogConstant.TAG, ImportWalletActivity.this.getString(R.string.wallet_creation_failed), e);
-                        return;
-                    }
+                    WalletFileUtils.createWalletFileAndLoad();
                 }
-
                 WalletContextHolder.get().initData();
 
                 Intent intent = new Intent(ImportWalletActivity.this, MainActivity.class);
@@ -165,10 +147,6 @@ public class ImportWalletActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==NOT_NOTICE){
-            //由于不知道是否选择了允许所以需要再次判断
-            myRequetPermission();
-        }
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUESTCODE_FROM_ACTIVITY) {
                 List<String> list = data.getStringArrayListExtra("paths");
@@ -196,71 +174,6 @@ public class ImportWalletActivity extends AppCompatActivity {
                             .setTitle(ImportWalletActivity.this.getString(R.string.dialog_title_error))
                             .setMessage(ImportWalletActivity.this.getString(R.string.current_selection_file_error))
                             .show();
-                }
-            }
-        }
-    }
-    private static final int NOT_NOTICE = 2;//如果勾选了不再询问
-    private AlertDialog alertDialog;
-    private AlertDialog mDialog;
-
-    private void myRequetPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }else {
-            Toast.makeText(this,"您已经申请了权限!",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 1) {
-            for (int i = 0; i < permissions.length; i++) {
-                if (grantResults[i] == PERMISSION_GRANTED) {//选择了“始终允许”
-                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])){//用户选择了禁止不再询问
-
-                        AlertDialog.Builder builder = new AlertDialog.Builder(ImportWalletActivity.this);
-                        builder.setTitle("permission")
-                                .setMessage("点击允许才可以使用我们的app哦")
-                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        if (mDialog != null && mDialog.isShowing()) {
-                                            mDialog.dismiss();
-                                        }
-                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                        Uri uri = Uri.fromParts("package", getPackageName(), null);//注意就是"package",不用改成自己的包名
-                                        intent.setData(uri);
-                                        startActivityForResult(intent, NOT_NOTICE);
-                                    }
-                                });
-                        mDialog = builder.create();
-                        mDialog.setCanceledOnTouchOutside(false);
-                        mDialog.show();
-
-
-
-                    }else {//选择禁止
-                        AlertDialog.Builder builder = new AlertDialog.Builder(ImportWalletActivity.this);
-                        builder.setTitle("permission")
-                                .setMessage("点击允许才可以使用我们的app哦")
-                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        if (alertDialog != null && alertDialog.isShowing()) {
-                                            alertDialog.dismiss();
-                                        }
-                                        ActivityCompat.requestPermissions(ImportWalletActivity.this,
-                                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                                    }
-                                });
-                        alertDialog = builder.create();
-                        alertDialog.setCanceledOnTouchOutside(false);
-                        alertDialog.show();
-                    }
-
                 }
             }
         }
--- app/src/main/java/net/bigtangle/wallet/activity/VerifyWalletActivity.java
+++ app/src/main/java/net/bigtangle/wallet/activity/VerifyWalletActivity.java
@@ -2,7 +2,10 @@ package net.bigtangle.wallet.activity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.yarolegovich.lovelydialog.LovelyInfoDialog;
+import com.yarolegovich.lovelydialog.LovelyStandardDialog;
 
 import net.bigtangle.wallet.R;
 import net.bigtangle.wallet.core.WalletContextHolder;
@@ -30,6 +34,8 @@ public class VerifyWalletActivity extends AppCompatActivity {
 
     @BindView(R.id.verify_wallet_button)
     Button verifyWalletButton;
+
+    private static final int NOT_NOTICE = 2; //如果勾选了不再询问
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,11 +95,77 @@ public class VerifyWalletActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NOT_NOTICE) {
+            //由于不知道是否选择了允许所以需要再次判断
+            requetPermission();
+        }
+    }
+
     private void requetPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         } else {
             Toast.makeText(this, "您已经申请了权限!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PERMISSION_GRANTED) {//选择了“始终允许”
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {//用户选择了禁止不再询问
+                        new LovelyStandardDialog(VerifyWalletActivity.this, LovelyStandardDialog.ButtonLayout.HORIZONTAL)
+                                .setTopColorRes(R.color.colorPrimary)
+                                .setButtonsColor(Color.WHITE)
+                                .setIcon(R.drawable.ic_error_white_24px)
+                                .setTitle(VerifyWalletActivity.this.getString(R.string.dialog_title_info))
+                                .setMessage("点击允许才可以使用我们的app哦")
+                                .setPositiveButton("去允许", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getPackageName(), null);//注意就是"package",不用改成自己的包名
+                                        intent.setData(uri);
+                                        startActivityForResult(intent, NOT_NOTICE);
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.cancel, new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                    }
+                                })
+                                .show();
+                    } else {//选择禁止
+                        new LovelyStandardDialog(VerifyWalletActivity.this, LovelyStandardDialog.ButtonLayout.HORIZONTAL)
+                                .setTopColorRes(R.color.colorPrimary)
+                                .setButtonsColor(Color.WHITE)
+                                .setIcon(R.drawable.ic_error_white_24px)
+                                .setTitle(VerifyWalletActivity.this.getString(R.string.dialog_title_info))
+                                .setMessage("点击允许才可以使用我们的app哦")
+                                .setPositiveButton("去允许", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        ActivityCompat.requestPermissions(VerifyWalletActivity.this,
+                                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                                    }
+                                })
+                                .setNegativeButton(android.R.string.cancel, new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                    }
+                                })
+                                .show();
+                    }
+                }
+            }
         }
     }
 }
