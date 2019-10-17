acbc83b88159c39a96afe323f1211b1853711151
jiangtao <jiangtao19890902@sina.com>
2019-10-17 21:26:54 +0800

    下载wallet文件

--- app/src/main/java/net/bigtangle/wallet/core/HttpService.java
+++ app/src/main/java/net/bigtangle/wallet/core/HttpService.java
@@ -48,9 +48,9 @@ public class HttpService {
         //
         //https://m.bigtangle.com.cn/vm/walletfiledownload?id=201905250100000005&userid=201905250100000004
 
-//        String url = "https://testcc.bigtangle.xyz/vm/walletfilepullout?signin=" + signin + "&password=" + password;
+        String url = "https://testcc.bigtangle.xyz/vm/walletfilepullout?signin=" + signin + "&password=" + password;
 
-        String url = "http://10.0.2.2:8080/cc/vm/walletfilepullout?signin=" + signin + "&password=" + password;
+//        String url = "http://10.0.2.2:8080/cc/vm/walletfilepullout?signin=" + signin + "&password=" + password;
 
         Request request = new Request.Builder()
                 .url(url)
