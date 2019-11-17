7a7c3f8599fea13f76ee311e897f195d8621ca79
jiangtao <jiangtao19890902@sina.com>
2019-11-17 19:14:24 +0800

    修改订单地址

--- app/src/main/java/net/bigtangle/wallet/activity/market/MarketSearchFragment.java
+++ app/src/main/java/net/bigtangle/wallet/activity/market/MarketSearchFragment.java
@@ -306,7 +306,7 @@ public class MarketSearchFragment extends BaseLazyFragment implements SwipeRefre
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction("android.intent.action.VIEW");
-                Uri content_url = Uri.parse("https://www.baidu.com/");//此处填链接
+                Uri content_url = Uri.parse("https://m.bigtangle.net/chartdata/index.html");//此处填链接
                 intent.setData(content_url);
                 startActivity(intent);
             }
