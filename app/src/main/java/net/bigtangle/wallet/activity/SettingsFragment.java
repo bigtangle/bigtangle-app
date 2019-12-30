a078745d0b1ba062a69b75931112670d23bb0d04
jiangtao <jiangtao19890902@sina.com>
2019-12-30 22:16:48 +0800

    pay

--- app/src/main/java/net/bigtangle/wallet/activity/SettingsFragment.java
+++ app/src/main/java/net/bigtangle/wallet/activity/SettingsFragment.java
@@ -1,7 +1,6 @@
 package net.bigtangle.wallet.activity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,39 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import net.bigtangle.core.ECKey;
-import net.bigtangle.core.Utils;
-import net.bigtangle.encrypt.ECIESCoder;
-import net.bigtangle.utils.BasicAuthInterceptor;
 import net.bigtangle.wallet.R;
 import net.bigtangle.wallet.activity.settings.SettingConnectionActivity;
 import net.bigtangle.wallet.activity.settings.SettingContactActivity;
 import net.bigtangle.wallet.activity.settings.SettingVersionActivity;
 import net.bigtangle.wallet.activity.settings.SettingWalletActivity;
 import net.bigtangle.wallet.components.BaseLazyFragment;
-import net.bigtangle.wallet.core.WalletContextHolder;
-
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import net.bigtangle.wallet.core.BrowserAccessTokenContext;
 
 import butterknife.BindView;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-import static net.bigtangle.utils.OkHttp3Util.contentHex;
-import static net.bigtangle.utils.OkHttp3Util.pubkey;
-import static net.bigtangle.utils.OkHttp3Util.signHex;
 
 public class SettingsFragment extends BaseLazyFragment {
 
@@ -126,21 +101,7 @@ public class SettingsFragment extends BaseLazyFragment {
                         @Override
                         public void run() {
                             try {
-                                ECKey ecKey = WalletContextHolder.get().walletKeys().get(0);
-                                OkHttpClient client = getUnsafeOkHttpClient();
-                                Request request = new Request.Builder().url("https://m.bigtangle.net/accessToken/generate?pubKey=" + ecKey.getPublicKeyAsHex()).get().build();
-                                Response response = client.newCall(request).execute();
-                                String accessToken = response.body().string();
-
-                                byte[] buf = Utils.HEX.decode(accessToken);
-                                byte[] bytes = ECIESCoder.decrypt(ecKey.getPrivKey(), buf);
-                                String verifyHex = new String(bytes);
-
-                                Intent intent = new Intent();
-                                intent.setAction("android.intent.action.VIEW");
-                                Uri content_url = Uri.parse("https://m.bigtangle.net/wallet/balance.jsf?accessToken=" + verifyHex);
-                                intent.setData(content_url);
-                                startActivity(intent);
+                                BrowserAccessTokenContext.get().open(getContext(), "https://m.bigtangle.net/wallet/balance.jsf");
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
@@ -148,37 +109,6 @@ public class SettingsFragment extends BaseLazyFragment {
                     }).start();
                 }
             });
-        }
-    }
-
-    private static OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            X509TrustManager tr = new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            };
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init((KeyManager[]) null, new TrustManager[]{tr}, (SecureRandom) null);
-            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-            OkHttpClient client = (new okhttp3.OkHttpClient.Builder()).sslSocketFactory(sslSocketFactory, tr).hostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            }).connectTimeout(timeoutMinute, TimeUnit.MINUTES).writeTimeout(timeoutMinute, TimeUnit.MINUTES).addInterceptor(new BasicAuthInterceptor(pubkey, signHex, contentHex)).readTimeout(timeoutMinute, TimeUnit.MINUTES).build();
-            return client;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 }
