1eb6ad642c9009c5c9de0920fe912f2ab4b870fb
tianj <mi@bigtangle.net>
2021-01-29 11:27:35 +0800

    identity

--- app/src/main/java/net/bigtangle/wallet/activity/wallet/WalletAccountIdentityFragment.java
+++ app/src/main/java/net/bigtangle/wallet/activity/wallet/WalletAccountIdentityFragment.java
@@ -94,7 +94,7 @@ public class WalletAccountIdentityFragment extends BaseLazyFragment implements S
         }
         Log.i(LogConstant.TAG, "initData adapter identityDatas.size()" + identityDatas.size());
         if (identityDatas != null && !identityDatas.isEmpty()) {
-            this.itemList = new ArrayList<WalletAccountIdentiyItem>();
+           // this.itemList = new ArrayList<WalletAccountIdentiyItem>();
             for (IdentityData identityData : identityDatas) {
                 WalletAccountIdentiyItem walletAccountIdentiyItem = new WalletAccountIdentiyItem();
                 walletAccountIdentiyItem.setName(identityData.getIdentityCore().getSurname());
