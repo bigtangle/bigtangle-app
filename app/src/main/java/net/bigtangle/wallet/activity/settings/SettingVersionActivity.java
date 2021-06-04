c724f82060789323d3453658e8477a3a34ddf82f
tianj <mi@bigtangle.net>
2021-06-04 15:19:38 +0800

    privacy

--- app/src/main/java/net/bigtangle/wallet/activity/settings/SettingVersionActivity.java
+++ app/src/main/java/net/bigtangle/wallet/activity/settings/SettingVersionActivity.java
@@ -1,11 +1,31 @@
 package net.bigtangle.wallet.activity.settings;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.bigtangle.wallet.R;
+import net.bigtangle.wallet.activity.PrivacyDialog;
+import net.bigtangle.wallet.activity.PrivacyPolicyActivity;
+import net.bigtangle.wallet.activity.SPUtil;
+import net.bigtangle.wallet.activity.SettingsFragment;
+import net.bigtangle.wallet.activity.TermsActivity;
+import net.bigtangle.wallet.activity.VerifyWalletActivity;
 import net.bigtangle.wallet.core.utils.UpdateUtil;
 
 import butterknife.BindView;
@@ -18,6 +38,8 @@ public class SettingVersionActivity extends AppCompatActivity {
 
     @BindView(R.id.version_text_view)
     TextView versionTextView;
+    @BindView(R.id.privacy_button)
+    Button privacyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +54,116 @@ public class SettingVersionActivity extends AppCompatActivity {
 
         String versionName = UpdateUtil.getVersionName(SettingVersionActivity.this);
         this.versionTextView.setText(versionName);
+        if (privacyButton != null) {
+            this.privacyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showPrivacy();
+                }
+            });
+        }
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
+    }
+
+    /**
+     * 显示用户协议和隐私政策
+     */
+    private void showPrivacy() {
+
+        final PrivacyDialog dialog = new PrivacyDialog(SettingVersionActivity.this);
+        TextView tv_privacy_tips = dialog.findViewById(R.id.tv_privacy_tips);
+        TextView btn_exit = dialog.findViewById(R.id.btn_exit);
+        TextView btn_enter = dialog.findViewById(R.id.btn_enter);
+        dialog.show();
+
+        String string = getResources().getString(R.string.privacy_tips);
+        String key1 = getResources().getString(R.string.privacy_tips_key1);
+        String key2 = getResources().getString(R.string.privacy_tips_key2);
+        int index1 = string.indexOf(key1);
+        int index2 = string.indexOf(key2);
+
+        //需要显示的字串
+        SpannableString spannedString = new SpannableString(string);
+        //设置点击字体颜色
+        ForegroundColorSpan colorSpan1 = new ForegroundColorSpan(getResources().getColor(R.color.selected_time_text));
+        spannedString.setSpan(colorSpan1, index1, index1 + key1.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        ForegroundColorSpan colorSpan2 = new ForegroundColorSpan(getResources().getColor(R.color.selected_time_text));
+        spannedString.setSpan(colorSpan2, index2, index2 + key2.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        //设置点击字体大小
+        AbsoluteSizeSpan sizeSpan1 = new AbsoluteSizeSpan(18, true);
+        spannedString.setSpan(sizeSpan1, index1, index1 + key1.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        AbsoluteSizeSpan sizeSpan2 = new AbsoluteSizeSpan(18, true);
+        spannedString.setSpan(sizeSpan2, index2, index2 + key2.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        //设置点击事件
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingVersionActivity.this, TermsActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                //点击事件去掉下划线
+                ds.setUnderlineText(false);
+            }
+        };
+        spannedString.setSpan(clickableSpan1, index1, index1 + key1.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingVersionActivity.this, PrivacyPolicyActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                //点击事件去掉下划线
+                ds.setUnderlineText(false);
+            }
+        };
+        spannedString.setSpan(clickableSpan2, index2, index2 + key2.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        //设置点击后的颜色为透明，否则会一直出现高亮
+        tv_privacy_tips.setHighlightColor(Color.TRANSPARENT);
+        //开始响应点击事件
+        tv_privacy_tips.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tv_privacy_tips.setText(spannedString);
+
+        //设置弹框宽度占屏幕的80%
+        WindowManager m = getWindowManager();
+        Display defaultDisplay = m.getDefaultDisplay();
+        final WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+        params.width = (int) (defaultDisplay.getWidth() * 0.80);
+        dialog.getWindow().setAttributes(params);
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                SPUtil.put(SettingVersionActivity.this, "sp_privacy", false);
+                finish();
+            }
+        });
+
+        btn_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+
+                SPUtil.put(SettingVersionActivity.this, "sp_privacy", true);
+
+                Toast.makeText(SettingVersionActivity.this, getString(R.string.confirmed), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
